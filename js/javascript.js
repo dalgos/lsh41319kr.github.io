16b14cafe6d9013450f82fedb7d20fc9b9ab19f6
leeseungha <seungha0802@gmail.com>
2016-10-12 18:11:21 +0900

    javascript (10/12)
    load()를 사용하여 기존의 문제점을 해결하려고 합니다.

--- js/javascript.js
+++ js/javascript.js
@@ -1,42 +1,4 @@
-//등장 애니메이션
-var appearAnimation = function () {
-	$('.text_wrp_titlebar').stop().animate({width : "56.8%"},300,function(){
-		$('.text_wrp_titlebar h1').stop().animate({marginTop: "40px"},300)
-	});
-}
-var mouseOut = function () {
-	if ($scrolly == 0){
-		$('.scroll_mouse img').stop().animate({marginTop : "0px"},400)	
-	}else{
-		$('.scroll_mouse img').animate({marginTop : "150px"},400)		
-	}
-}
-//textBarAnimation
-var isHalf = false;
-var textBarAnimation = function () {
-	var $titleWrpH = $('.title_wrp').height();
-	var $videoWrpH = $('.video_wrp').height();
-	var $textWrpTitleBar = $('.text_wrp_titlebar').height();
-	
-	/* 애니매이션이 꼬인다. */
-	if( $scrolly > $titleWrpH - ($textWrpTitleBar / 2) && $scrolly < $titleWrpH + $videoWrpH){
-		isHalf = true;
-		$('.text_wrp_titlebar').stop().animate({top: "100%"},400)
-		$('.video_bg').stop().animate({marginTop:'0%'},400,function(){
-			$('.video img').stop().animate({opacity : "1"})
-		})
-		console.log(isHalf)
-	}
-	else{
-		if(isHalf){
-			$('.text_wrp_titlebar').stop().animate({top: "40%"},400)	
-			$('.video img').stop().animate({opacity : "0"},function(){
-				$('.video_bg').stop().animate({marginTop:'-100%'},400)
-			})	
-			return false;
-		}	
-	}
-}
+/* ================= GNB Function ================= */
 //navBtnClick animation
 var navBtnClick = function(){
 	$('.btn_menu').click(function(){
@@ -106,13 +68,41 @@ var navMenuClick = function () {
 		$('.menu_img li:last-child').find('img').stop().animate({opacity : 1},150)
 	})
 }
+/* ================= Appearance Animation ================= */
+var $appearanceAnimation = function(){
+	var beHalf = true
+	if ($scrolly != 0){
+		if (beHalf)
+			{
+				$('.title_bar').stop().animate({marginTop : '0%'},300)	
+			}
+		beHalf = false;
+	}
+}
+/* ================= Title Function ================= */
+var isHalf = true
+var $dd = function(){
+	if($scrolly > $windowy / 2){
+		if(isHalf){
+			$('.container').css({
+				zIndex : '3',
+				position : 'absolute',
+				top : '100%'
+			}).stop().animate({top : '1250px'},200,function(){
+				$('.container_inner').load("video.html #video").animate({height : '585px'},300)
+			});
+		}
+	isHalf= false;
+	}
+}
 // when? scroll
 $(window).scroll(function(){
 	$scrolly = $(window).scrollTop();
 	$windowy = $(window).height();
-	mouseOut();
-	textBarAnimation();
 	console.log("windowy" + ":" + $windowy + " " +  "scrolly" + ":" +  $scrolly)
+	
+	$dd();
+	$appearanceAnimation();
 })
 // when? resize
 $(window).resize(function(){
@@ -122,22 +112,22 @@ $(window).resize(function(){
 })
 // when? document ready
 $(document).ready(function(){
-	appearAnimation();
+
 })
 // when? window load
 $(window).load(function(){
 	$scrolly = $(window).scrollTop();
 	$windowy = $(window).height();
-	$('nav div.wrp').css('height', $windowy)
-
-	$bubble_width_1 = $('.bubble1_wrp_1').width();
+	$('nav div.gnb_container').css('height', $windowy)
+	console.log("windowy" + ":" + $windowy + " " +  "scrolly" + ":" +  $scrolly)
+	/*$bubble_width_1 = $('.bubble1_wrp_1').width();
 	$bubble_width_2 = $('.bubble1_wrp_2').width();
 	$bubble_width_3 = $('.bubble1_wrp_3').width();
 	$bubble_width_4 = $('.bubble1_wrp_4').width();
 	$bubble_width_5 = $('.bubble1_wrp_5').width();
-	console.log("bubble1" + ":" + $bubble_width_1 + "/" + "bubble2" + ":" + $bubble_width_2 + "/" +"bubble3" + ":" + $bubble_width_3 + "/" +"bubble4" + ":" + $bubble_width_4 + "/" +"bubble5" + ":" + $bubble_width_5 + "/")
+	console.log("bubble1" + ":" + $bubble_width_1 + "/" + "bubble2" + ":" + $bubble_width_2 + "/" +"bubble3" + ":" + $bubble_width_3 + "/" +"bubble4" + ":" + $bubble_width_4 + "/" +"bubble5" + ":" + $bubble_width_5 + "/")*/
 	
-	mouseOut();
 	navBtnClick();
 	navMenuClick();
+	$appearanceAnimation();
 })
