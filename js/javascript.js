e97b011f7d97b2809eb6e2b1be6fff114cbd41d2
leeseungha <seungha0802@gmail.com>
2016-10-15 00:05:00 +0900

    javascript (10/15)
    하나의 div로 다른 컨텐츠들을 넣다뺏다하는것을 스크롤에 따라서 제어하는 동작이 어느정도 완료 되었지만 완벽하지는 않다.

--- js/javascript.js
+++ js/javascript.js
@@ -84,7 +84,7 @@ var $appearanceAnimation = function(){
 	}
 }
 /* ================= Title Function ================= */
-var isHalf = true
+var isHalf = true;
 var $dd = function(){
 		var $titleH = $('.title_wrp_bar_bg').height();
 		var $videoH = $('.video_wrp_bg').height();
@@ -92,47 +92,144 @@ var $dd = function(){
 		var $composerH = $('.composer_wrp_bg').height();
 		var $ostBarH = $('.ost_wrp_bg_bar').height();
 		var $ostH = $('.ost_wrp_bg_').height();
-	if($scrolly > $titleH / 2 && $scrolly < $titleH + ($videoH / 2)){
-		if(isHalf){
-			// ============ Qusetion isHalf가 true라는 조건을 걸어 놓으면 =============================
-			$('.title_bar').animate({
-				marginTop : '-100%',
-				opacity : '0'
-			},200)// ====== .title_bar의 걸어 놓은 애니메이션이 작동하지 않습니다. isHalf에 대한 조건이 없으면 정상 작동 하고 이 상태를 cosole에서 애니메이션을 걸어도 정상 작동합니다.
-			$('.container').css({
-				zIndex : '3',
-				position : 'absolute',
-				top : '100%'
-			}).animate({top : '1250px'},200,function(){
-			$('.container_inner').load("video.html #video").animate({height : '585px'},300)
-			});
-			isHalf = false;
+	if($('#body').hasClass('scroll_down')){
+		if($scrolly > $titleH / 2 && $scrolly < $titleH + ($videoH / 2)){
+			if(isHalf){
+				//console.log('gogo1') // ============ Question 이 조건문에서는 원하는대로 한번만 실행되지만 아래 esle if 부분에서는 여러번 실행이됩니다. 왜그런걸까요? ===========
+				//if문의 조건을 다르게 해서 한번만 작동하게 하게 하였는데 이유는 대충 유추 할수있지만 명확히 모르겠습니다. 문제는 새로고침을 한다면 제대로 작동하지 않는 부분이 생깁니다. 
+				// ============ Qusetion isHalf가 true라는 조건을 걸어 놓으면 =============================
+				$('.title_bar').animate({
+					marginTop : '-100%',
+					opacity : '0'
+				},200)// ====== .title_bar의 걸어 놓은 애니메이션이 작동하지 않습니다. isHalf에 대한 조건이 없으면 정상 작동 하고 이 상태를 cosole에서 애니메이션을 걸어도 정상 작동합니다.
+				$('.container').css({
+					zIndex : '3',
+					position : 'absolute',
+					top : '100%'
+				}).animate({top : '1250px'},200,function(){
+				$('.container_inner').load("video.html #video").css({bottom : 0,top : ''}).animate({height : '585px'},300)
+				});
+				isHalf = false;
+			}	
 		}
-	}
-	else if ($scrolly >= $titleH + ($videoH / 2) && $scrolly <$titleH + $videoH + ($composerBarH / 2))
-	{	
-		isHalf =true;
-		if(isHalf){
-			$('.title_bar').animate({
-				marginTop : '0%',
-				opacity : '1'
-			},200)
-			$('.container').css({
-			position : 'fixed',
-			top : '50%'
-			})
-			$('.container_inner').load("index.html .title_bar").animate({height : "165px"},function(){
-				$('.title_bar').children('h1').text('composer')
-				$('.title_bar').children('p').text('작곡가')	
-			});
+		else if ($scrolly >= $titleH + ($videoH / 2) && $scrolly < $titleH + $videoH + ($composerBarH / 2)){
+			if(!isHalf){
+				console.log('gogo2')
+				$('.title_bar').animate({
+					marginTop : '0%',
+					opacity : '1'
+				},200)
+				$('.container').css({
+				position : 'fixed',
+				top : '50%'
+				})
+				$('.container_inner').load("index.html .title_bar").animate({height : "165px"},function(){
+					$('.title_bar').children('h1').text('composer')
+					$('.title_bar').children('p').text('작곡가')	
+				});
+				isHalf = true;
+			}	
+		}
+		else if ($scrolly >= $titleH + $videoH + ($composerBarH / 2) && $scrolly < $titleH + $videoH + $composerBarH + ($composerH / 2)){
+			if (isHalf){
+				console.log('gogo3')
+				$('.composer_wrp_bar_bg').css({zIndex : 4})
+				$('.title_bar').animate({
+					marginTop : '-100%',
+					opacity : '0'
+				},200)
+				$('.container').css({
+					zIndex : '3',
+					position : 'absolute',
+					top : '270%'
+				}).animate({top : '2650px'},200,function(){
+				$('.container_inner').animate({width : '85%'},200,function(){$('.container_inner').animate({height : '585px'},300).css({bottom:0,top : ''})})});
+				$('.container_inner').load("composer.html #composer")
+				isHalf = false;
+			}	
+		}
+		else if ($scrolly >= $titleH + $videoH + $composerBarH + ($composerH / 2) && $titleH + $videoH + $composerBarH + $composerH + ($composerH / 2)){
+			if(!isHalf){
+				$('.title_bar').animate({
+					marginTop : '0%',
+					opacity : '1'
+				},200)
+				$('.container').css({
+				position : 'fixed',
+				top : '50%',
+				left : 0,
+				right : 0,
+				marginLeft : '5%',
+				width : "95%"
+				});
+				$('.container_inner').load("index.html .title_bar").animate({width : "70%",height : "165px"},function(){
+					$('.title_bar').children('h1').text('Another OST')
+					$('.title_bar').children('p').text('하울의 움직이는 성 OST')	
+				});
+				isHalf = true;
+			}	
+		}
+	}else if ($('#body').hasClass('scroll_up')){
+		if($scrolly < $titleH / 2){
 			console.log('gogo')
-			isHalf = false;
+			if(!isHalf){
+				$('.title_bar').animate({
+					marginTop : '0%',
+					opacity : '1'
+				},200)
+				$('.container').css({
+					zIndex : '5',
+					position : 'fixed',
+					top : '50%',
+				});
+				$(".container_inner").load("index.html .title_bar").css({top : 0,marginTop : '-82.5px'}).animate({height : '165px'},300)
+				isHalf = true;
+			}
+		}
+		else if ($scrolly <= $titleH + ($videoH / 2) && $scrolly >= $titleH / 2){
+			if(isHalf){
+				$('.title_bar').animate({
+					marginTop : "100%",
+					opacity : '0'
+				})
+				$('.container').css({
+					position : 'absolute',
+					top: "158%"
+				})
+				$('.container_inner').load("video.html #video").css({top: '',bottom:'0'}).animate({height : '585px'},300)
+				isHalf = false;
+			}
+		}
+		else if ($scrolly <= $titleH + $videoH + ($composerBarH / 2) && $scrolly > $titleH + ($videoH / 2) ){
+			if (!isHalf){
+				$('.composer_wrp_bar_bg').css({zIndex : 1})
+				$('.title_bar').animate({marginTop : "0%",opacity : "1"},200)
+				$('.container').css({position : 'fixed', top : "50%",left : 0,right:0,marginLeft : "5%",width :"95%"})
+				$('.container_inner').load("index.html .title_bar").css({top : '0',bottom: ''}).animate({width : "70%",height : "165px",marginTop : "-82.5px"},function(){
+					$('.title_bar').children('h1').text('composer')
+					$('.title_bar').children('p').text('작곡가')	
+				});
+				isHalf = true;
+			}
+		}
+		else if ($scrolly <= $titleH + $videoH + $composerBarH + ($composerH / 2) && $scrolly > $titleH + $videoH + ($composerBarH / 2)){
+			if (isHalf){
+				$('.composer_wrp_bar_bg').css({zIndex : 1})
+				$('.title_bar').animate({marginTop : "100%",opacity : '0'})
+				$('.container').css({position : 'absolute',top : '2650px'})
+				$('.container_inner').animate({width : '85%'},200,function(){$('.container_inner').animate({height : '585px'},300)}).load("composer.html #composer")
+				isHalf = false;
+			}
 		}
 	}
 }
 // when? scroll
 var scrollPosition = $(window).scrollTop(); //Question scrollPosition과 $scrolly은 똑같이 $(window).scrollTop();을 담았는데 왜 값이 차이가 날까요?
 $(window).scroll(function(){
+	$scrolly = $(window).scrollTop();
+	$windowy = $(window).height();
+	// GNB nav에 높이값 지정
+	$('nav div.gnb_container').css('height', $windowy)
 	//scroll UP & Down
 	if ($scrolly > scrollPosition){
 		console.log('scrollPosition' + scrollPosition + ' ' + '$scrolly' + $scrolly)
@@ -146,8 +243,6 @@ $(window).scroll(function(){
 	}
 	scrollPosition = $scrolly;
 	//scroll UP & Down End
-	$scrolly = $(window).scrollTop();
-	$windowy = $(window).height();
 	
 	$dd();
 	$appearanceAnimation();
@@ -156,6 +251,8 @@ $(window).scroll(function(){
 $(window).resize(function(){
 	$scrolly = $(window).scrollTop();
 	$windowy = $(window).height();
+	// GNB nav에 높이값 지정
+	$('nav div.gnb_container').css('height', $windowy)
 	$('nav div.wrp').css('height', $windowy)
 })
 // when? document ready
@@ -166,6 +263,7 @@ $(document).ready(function(){
 $(window).load(function(){
 	$scrolly = $(window).scrollTop();
 	$windowy = $(window).height();
+	// GNB nav에 높이값 지정
 	$('nav div.gnb_container').css('height', $windowy)
 	console.log("windowy" + ":" + $windowy + " " +  "scrolly" + ":" +  $scrolly)
 	/*$bubble_width_1 = $('.bubble1_wrp_1').width();
